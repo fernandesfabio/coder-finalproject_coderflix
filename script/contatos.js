446c8604f6da03b3391dfb19defbea9cbe65dec5
Anna-csn <annaclarasnascimento@hotmail.com>
2023-11-14 23:58:28 -0300

    criando o evento aplicando o alert ao código

---
+++ script/contatos.js
@@ -0,0 +1,12 @@
+
+let enviar = document.getElementById("botenviar");
+enviar.addEventListener("click", mensagem)
+function mensagem(){
+  alert("Mensagem enviada com sucesso, a Coderflix agradece o seu contato!");
+}
+
+
+
+  
+  
+
